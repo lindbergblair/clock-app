58726426890a136084bbb312d232b100da1dbcc4
bencassidy <ben.cassidy11@gmail.com>
2016-01-12 21:22:27 -0800

    server running

---
+++ server.js
@@ -0,0 +1,32 @@
+var hapi = require('hapi');
+var inert = require('inert');
+var moment = require('moment');
+
+var server = new hapi.Server();
+server.connection({ port: 3000 });
+
+server.start(function () {
+    console.log('Server running at:', server.info.uri);
+});
+
+server.register(inert, function (err) {
+    if (err) {
+        throw err;
+    }
+
+    server.route({
+        method: 'GET',
+        path: '/hello',
+        handler: function handler(request, reply) {
+            reply.file('./public/index.html');
+        }
+    });
+});
+
+server.route({
+    method: 'GET',
+    path: '/time',
+    handler: function (request, reply) {
+        reply(moment().format('MMMM Do YYYY, h:mm:ss a'));
+    }
+});
